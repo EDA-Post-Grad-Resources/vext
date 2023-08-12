cd5c1c1396bf56df1f0b8b69378bd4a4533cea54
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-12 21:14:46 +1200

    improve styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       addUtilities({
         ".diagonal-lines": {
           background:
-            "repeating-linear-gradient(45deg, rgba(0,0,0,1) 1px, rgba(0,60,0,1) 2px )",
+            "repeating-linear-gradient(180deg, rgba(0,0,0,1) 1px, rgba(0,30,0,1) 20px )",
         },
       });
     },
