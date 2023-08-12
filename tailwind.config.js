5a269a0f504862301b0fbe4a0dcf19735a8ac2f2
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-13 11:18:38 +1200

    remove background

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        ".diagonal-lines": {
-          background:
-            "repeating-linear-gradient(180deg, rgba(0,0,0,1) 1px, rgba(0,30,0,1) 20px )",
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
