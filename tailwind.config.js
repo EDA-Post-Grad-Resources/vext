6e3dc3dd0dfd2bb0eb96ae3a07d34a3dfb5ac3ab
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-12 19:07:08 +1200

    install google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        caveat: ["Caveat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
