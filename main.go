639edf1091b9ebebcdeae1a1b548e2a4afac48f7
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-13 12:22:53 +1200

    refactor broadcast into its own function

--- main.go
+++ main.go
@@ -7,52 +7,15 @@ import (
 	"html/template"
 	"log"
 	"time"
+
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 )
 
-func main() {
-	r := gin.Default()
-	m := melody.New()
-
-	r.Use(static.Serve("/public", static.LocalFile("./public", true)))
-
-	// Load templates from the "templates" directory
-	r.LoadHTMLGlob("templates/*")
-
-	r.GET("/ws", func(c *gin.Context) {
-		m.HandleRequest(c.Writer, c.Request)
-	})
-
-	m.HandleConnect(func(s *melody.Session) {
-		// generate a random user ID
-		userId := gofakeit.Name()
-		// set the user ID on the session
-		s.Set("userId", userId)
-		fmt.Println(s.Request.RemoteAddr + " has been assigned to " + userId)
-	})
-
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
-
-		// parse the message into an object that has userMessage property
-		var userMessage struct {
-			Content string `json:"userMessage"`
-		}
-		// unmarshal the message into the object
-		err := json.Unmarshal(msg, &userMessage)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		// Get the user ID from the session
-		userId, exists := s.MustGet("userId").(string)
-		if !exists {
-			log.Fatal("User ID not found")
-			return
-		}
+func broadcast(m *melody.Melody, action string, userId string, msg string) {
+	
 		// Define the dynamic data
 		data := struct {
 			User    string
@@ -60,8 +23,8 @@ func main() {
 			Color   string
 			Time    string
 		}{
-			User:    userId,
-			Content: userMessage.Content,
+			User:		userId,
+			Content: msg,
 			Color:   stringToHue(userId),
 			Time:    time.Now().Format("15:04:05"),
 		}
@@ -83,6 +46,52 @@ func main() {
 
 		// Broadcast the rendered HTML
 		m.Broadcast(renderedHTML.Bytes())
+}
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.Use(static.Serve("/public", static.LocalFile("./public", true)))
+
+	// Load templates from the "templates" directory
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleConnect(func(s *melody.Session) {
+		// generate a random user ID
+		userId := gofakeit.Name()
+		// set the user ID on the session
+		s.Set("userId", userId)
+		fmt.Println(s.Request.RemoteAddr + " has been assigned to " + userId)
+
+		broadcast(m, "join", userId, "has joined the chat")
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+
+		// parse the message into an object that has userMessage property
+		var userMessage struct {
+			Content string `json:"userMessage"`
+		}
+		// unmarshal the message into the object
+		err := json.Unmarshal(msg, &userMessage)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		// Get the user ID from the session
+		userId, exists := s.MustGet("userId").(string)
+		if !exists {
+			log.Fatal("User ID not found")
+			return
+		}
+		
+		broadcast(m,"message", userId, userMessage.Content)
 	})
 
 	r.GET("/", func(c *gin.Context) {
